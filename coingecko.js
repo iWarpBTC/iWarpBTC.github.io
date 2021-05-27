850507d99ce3ffcf3ba9df60ddec4e92fe39d528
iWarpBTC <iwarp@tuta.io>
2021-05-27 22:38:17 +0200

    ticker update

--- coingecko.js
+++ coingecko.js
@@ -15,14 +15,30 @@ getapi(api_url);
 function show(data) {
   satsperczk = (1 / data.bitcoin.czk * 100000000).toFixed(0);
   czkpersats = (data.bitcoin.czk / 100000000).toFixed(0);
-  h2 = document.createElement('h2')
+  h2 = document.createElement('h2');
   h2.className = 'price';
   if (czkpersats >= 1)
-    h2.innerHTML = `${czkpersats} CZK/sat`;
+  {
+    mena = 'korun';
+    if (czkpersats == 1)
+      mena = 'koruna';
+    else if (czkpersats < 5)
+      mena = 'koruny';
+    h2.innerHTML = `<span>${czkpersats} ${mena} za satoshi</span>`;
+  }
   else
-    h2.innerHTML = `${satsperczk} sats/CZK`;
+  {
+    h2.innerHTML = `<span>${satsperczk} satoshi za korunu</span>`;
+  }
 
   div = document.getElementById('satoshidiv');
   before = document.getElementById('satoshi');
   div.insertBefore(h2, before);
+
+  img = document.createElement('img');
+  img.src = 'https://btc-slovnik.cz/img/tag.png';
+  img.height = 25;
+  img.alt = "cena";
+  img.className = "pricetag";
+  h2.insertBefore(img, h2.firstChild);
 }
